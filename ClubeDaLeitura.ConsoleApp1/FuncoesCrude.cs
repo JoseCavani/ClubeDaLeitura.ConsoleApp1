d8146b6957bad7dd5a9384427621b21e0a8cbd5e
Jose Cavani <Jose.Cavani@gmail.com>
2022-03-16 20:41:16 -0300

    commit

--- ClubeDaLeitura.ConsoleApp1/FuncoesCrude.cs
+++ ClubeDaLeitura.ConsoleApp1/FuncoesCrude.cs
@@ -6,7 +6,6 @@ namespace ClubeDaLeitura.ConsoleApp1
     {
         public class FuncoesCrude
         {
-           
             public void MostrarEmprestimos(GerenciadorEmprestimo gerenciadorEmprestimo, bool abertos)
             {
                 if (abertos)
@@ -21,6 +20,39 @@ namespace ClubeDaLeitura.ConsoleApp1
 
             public void RegistraEmprestimos(GerenciadorPessoa gerenciadorPessoas, GerenciadorRevista gerenciadorRevista, GerenciadorEmprestimo gerenciadorEmprestimo, Revista[] revistas, Pessoa[] pessoas, AcharPosicao acharPosicao, Emprestimo[] emprestimos)
             {
+                Mensagen mensagen = new();
+                 bool haRevistas = false;
+                foreach (var item in revistas)
+                {
+                    if (item != null && item.disponivel == true)
+                    {
+                        haRevistas = true;
+                        break;
+                    }
+                }
+                if (haRevistas == false)
+                {
+                    mensagen.Erro("não ha revistas disponiveis");
+                    return;
+                }
+
+                bool haPessoas = false;
+                foreach (var item in pessoas)
+                {
+                    if (item != null && item.temEmprestimo == false)
+                    {
+                        haPessoas = true;
+                        break;
+                    }
+                }
+                if (haPessoas == false)
+                {
+                    mensagen.Erro("não ha pessoas para emprestar disponiveis");
+                    return;
+                }
+
+
+
                 int posicao = acharPosicao.AcharPosicaoNulo(emprestimos);
                 emprestimos[posicao] = new Emprestimo();
                 gerenciadorEmprestimo.Registrar(gerenciadorPessoas, gerenciadorRevista, revistas, pessoas, posicao);
@@ -28,6 +60,22 @@ namespace ClubeDaLeitura.ConsoleApp1
 
             public void Editar(dynamic gerenciador,Menu menu, dynamic[] objeto)
             {
+                Mensagen mensagen = new();
+                bool haObjetos = false;
+                foreach (var item in objeto)
+                {
+                    if(item != null)
+                    {
+                        haObjetos = true;
+                        break;
+                    }
+                }
+                if(haObjetos == false)
+                {
+                    mensagen.Erro("não ha nada para editar");
+                    return;
+                }
+
                 Mostrar(gerenciador);
                 int numeroEditar = menu.EditarQual(objeto);
                 Console.Clear();
@@ -35,6 +83,22 @@ namespace ClubeDaLeitura.ConsoleApp1
             }
             public void EditarEmprestimo(dynamic gerenciador, Menu menu, dynamic[] objeto, Pessoa[] pessoas, Revista[] revista)
             {
+                Mensagen mensagen = new();
+                bool haObjetos = false;
+                foreach (var item in objeto)
+                {
+                    if (item != null)
+                    {
+                        haObjetos = true;
+                        break;
+                    }
+                }
+                if (haObjetos == false)
+                {
+                    mensagen.Erro("não ha nada para editar");
+                    return;
+                }
+
                 Mostrar(gerenciador);
                 int numeroEditar = menu.EditarQual(objeto);
                 Console.Clear();
@@ -54,6 +118,39 @@ namespace ClubeDaLeitura.ConsoleApp1
 
             public void EditarRevista(GerenciadorRevista gerenciadorRevista, Menu menu, Revista[] revista, Caixa[] caixa)
             {
+
+                Mensagen mensagen = new();
+                bool haCaixa = false;
+                foreach (var item in caixa)
+                {
+                    if (caixa != null)
+                    {
+                        haCaixa = true;
+                        break;
+                    }
+                }
+                if (haCaixa == false)
+                {
+                    mensagen.Erro("não ha caixas");
+                    return;
+                }
+
+                bool haRevista = false;
+                foreach (var item in revista)
+                {
+                    if (revista != null)
+                    {
+                        haRevista = true;
+                        break;
+                    }
+                }
+                if (haRevista == false)
+                {
+                    mensagen.Erro("não ha caixas");
+                    return;
+                }
+
+
                 Mostrar(gerenciadorRevista);
                 int numeroEditar = menu.EditarQual(revista);
                 Console.Clear();
@@ -61,6 +158,21 @@ namespace ClubeDaLeitura.ConsoleApp1
             }
             public void ExcluirEmprestimo(GerenciadorCaixa gerenciadorCaixa, GerenciadorEmprestimo gerenciadorEmprestimo, Emprestimo[] emprestimo, Mensagen mensagen, Pessoa[] pessoas, Revista[] revistas)
             {
+                bool haEmprestimos = false;
+                foreach (var item in emprestimo)
+                {
+                    if(emprestimo != null)
+                    {
+                        haEmprestimos = true;
+                        break;
+                    }
+                }
+                if ( haEmprestimos == false)
+                {
+                    mensagen.Erro("não ha emprestimos");
+                    return;
+                }
+
                 bool houveErro = false;
                 Mensagen mensagens = new Mensagen();
                 int posicao;
@@ -110,6 +222,22 @@ namespace ClubeDaLeitura.ConsoleApp1
             }
             public void Excluir(dynamic gerenciador,dynamic[] objeto, Mensagen mensagen)
             {
+                bool haObjeto = false;
+
+                foreach (var item in objeto)
+                {
+                    if (item != null)
+                    {
+                        haObjeto = true;
+                        break;
+                    }
+                }
+                if (haObjeto == false)
+                {
+                    mensagen.Erro("não ha nada para excluir");
+                        return;
+                }
+
                 Mostrar(gerenciador);
                 int posicaoExluir = mensagen.Excluir(objeto, "qual o ID que deseja excluir");
 
@@ -122,6 +250,27 @@ namespace ClubeDaLeitura.ConsoleApp1
 
             public void RegistrarNovaRevista(GerenciadorCaixa gerenciadorCaixa, GerenciadorRevista gerenciadorRevista, Revista[] revista, Caixa[] caixa, AcharPosicao acharPosicao)
             {
+                Mensagen mensagen = new();
+               
+
+                bool haCaixa = false;
+                foreach (var item in caixa)
+                {
+                    if (item != null)
+                    {
+                        haCaixa = true;
+                        break;
+                    }
+                }
+                if (haCaixa == false)
+                {
+                    mensagen.Erro("não ha caixas disponiveis");
+                    return;
+                }
+
+
+
+
                 int posicao = acharPosicao.AcharPosicaoNulo(revista);
                 revista[posicao] = new();
                 gerenciadorRevista.Registar(gerenciadorCaixa, posicao, caixa);
