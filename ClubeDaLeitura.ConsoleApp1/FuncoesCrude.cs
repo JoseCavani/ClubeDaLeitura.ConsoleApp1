ddf2efd229b73127d0b735f15ce077b5c21530a4
Jose Cavani <Jose.Cavani@gmail.com>
2022-03-16 12:52:07 -0300

    commit

---
+++ ClubeDaLeitura.ConsoleApp1/FuncoesCrude.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace ClubeDaLeitura.ConsoleApp1
+{
+    internal partial class Program
+    {
+        public class FuncoesCrude
+        {
+            public void MostrarEmprestimos(Emprestimo[] emprestimos, bool abertos)
+            {
+                if (abertos)
+                {
+                    for (int i = 0; i < emprestimos.Length; i++)
+                    {
+                        if (emprestimos[i] == null || emprestimos[i].aberto == false)
+                            continue;
+                        Console.WriteLine($"ID : {i}");
+                        emprestimos[i].Mostrar();
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < emprestimos.Length; i++)
+                    {
+                        if (emprestimos[i] == null)
+                            continue;
+                        TimeSpan diff = DateTime.Today - emprestimos[i].dataEmprestimo;
+                        if (diff.Days > 30)
+                            continue;
+                        Console.WriteLine($"ID : {i}");
+                        emprestimos[i].Mostrar();
+                    }
+                }
+                Console.ReadKey();
+            }
+
+            public void RegistraEmprestimos(Revista[] revistas, Pessoa[] pessoas, AcharPosicao acharPosicao, Emprestimo[] emprestimos)
+            {
+                int posicao = acharPosicao.AcharPosicaoNulo(emprestimos);
+                emprestimos[posicao] = new Emprestimo();
+                emprestimos[posicao].Registrar(revistas, pessoas);
+            }
+
+            public void Editar(Menu menu, dynamic[] objeto)
+            {
+                Mostrar(objeto);
+                int numeroEditar = menu.Editar(objeto);
+                Console.Clear();
+                objeto[numeroEditar].Editar();
+            }
+
+            public void Registrar(dynamic[] objeto, AcharPosicao acharPosicao)
+            {
+                int posicao = acharPosicao.AcharPosicaoNulo(objeto);
+
+                if (objeto is Caixa[])
+                    objeto[posicao] = new Caixa();
+                else if (objeto is Pessoa[])
+                    objeto[posicao] = new Pessoa();
+                objeto[posicao].Registrar();
+            }
+
+            public void EditarRevista(Menu menu, Revista[] revista, Caixa[] caixa)
+            {
+                Mostrar(revista);
+                int numeroEditar = menu.Editar(revista);
+                Console.Clear();
+                revista[numeroEditar].EditarRevista(caixa);
+            }
+            public void ExcluirEmprestimo(Emprestimo[] emprestimo, Mensagen mensagen, Pessoa[] pessoas, Revista[] revistas)
+            {
+                Mostrar(emprestimo);
+                int posicaoExluir = mensagen.Excluir(emprestimo, "qual o ID que deseja excluir");
+
+                emprestimo[posicaoExluir].aberto = false;
+                emprestimo[posicaoExluir].dataDevolucao = DateTime.Now;
+                foreach (var item in pessoas)
+                {
+                    if (item == null)
+                        continue;
+                    if (item.nome == emprestimo[posicaoExluir].amigo.nome)
+                    {
+                        item.temEmprestimo = false;
+                        break;
+                    }
+                }
+                foreach (var item in revistas)
+                {
+                    if (item == null)
+                        continue;
+                    if (item.numeroEdicao == emprestimo[posicaoExluir].revista.numeroEdicao)
+                    {
+                        item.disponivel = true;
+                        break;
+                    }
+                }
+                mensagen.Sucesso("fechado com sucesso");
+
+            }
+            public void Excluir(dynamic[] objeto, Mensagen mensagen)
+            {
+                Mostrar(objeto);
+                int posicaoExluir = mensagen.Excluir(objeto, "qual o ID que deseja excluir");
+
+
+                objeto[posicaoExluir] = null;
+                mensagen.Sucesso("removido com sucesso");
+
+                Console.ReadKey();
+            }
+
+            public void RegistrarNovaRevista(Revista[] revista, Caixa[] caixa, AcharPosicao acharPosicao)
+            {
+                int posicao = acharPosicao.AcharPosicaoNulo(revista);
+                revista[posicao] = new();
+                revista[posicao].Registar(caixa);
+            }
+
+            public void Mostrar(dynamic[] objeto)
+            {
+                for (int i = 0; i < objeto.Length; i++)
+                {
+                    if (objeto[i] == null)
+                        continue;
+                    Console.WriteLine($"ID : {i}");
+                    objeto[i].Mostrar();
+                }
+
+                Console.ReadKey();
+            }
+        }
+      
+    }
+}
--- ClubeDaLeitura.ConsoleApp1/Program.cs
+++ ClubeDaLeitura.ConsoleApp1/Program.cs
@@ -15,6 +15,7 @@ namespace ClubeDaLeitura.ConsoleApp1
             AcharPosicao acharPosicao = new();
             Mensagen mensagen = new();
             Emprestimo[] emprestimos = new Emprestimo[100];
+            FuncoesCrude funcaoCrude = new();
             #endregion
 
             #region valores
@@ -47,16 +48,16 @@ namespace ClubeDaLeitura.ConsoleApp1
                         switch (menu.MenuSecundarioGeral())
                         {
                             case 1:
-                                RegistrarNovaRevista(revistas, caixas, acharPosicao);
+                                funcaoCrude.RegistrarNovaRevista(revistas, caixas, acharPosicao);
                                 break;
                             case 2:
-                                Mostrar(revistas);
+                                funcaoCrude.Mostrar(revistas);
                                 break;
                             case 3:
-                                Excluir(revistas, mensagen);
+                                funcaoCrude.Excluir(revistas, mensagen);
                                 break;
                             case 4:
-                                EditarRevista(menu, revistas, caixas);
+                                funcaoCrude.EditarRevista(menu, revistas, caixas);
                                 break;
                             case 5:
                                 break;
@@ -70,16 +71,16 @@ namespace ClubeDaLeitura.ConsoleApp1
                         switch (menu.MenuSecundarioGeral())
                         {
                             case 1:
-                                Registrar(caixas, acharPosicao);
+                                funcaoCrude.Registrar(caixas, acharPosicao);
                                 break;
                             case 2:
-                                Mostrar(caixas);
+                                funcaoCrude.Mostrar(caixas);
                                 break;
                             case 3:
-                                Excluir(caixas,mensagen);
+                                funcaoCrude.Excluir(caixas,mensagen);
                                 break;
                             case 4:
-                                Editar(menu, caixas);
+                                funcaoCrude.Editar(menu, caixas);
                                 break;
                             case 5:
                                 break;
@@ -93,16 +94,16 @@ namespace ClubeDaLeitura.ConsoleApp1
                         switch (menu.MenuSecundarioGeral())
                         {
                             case 1:
-                                Registrar(pessoas, acharPosicao);
+                                funcaoCrude.Registrar(pessoas, acharPosicao);
                                 break;
                             case 2:
-                                Mostrar(pessoas);
+                                funcaoCrude.Mostrar(pessoas);
                                 break;
                             case 3:
-                                Excluir(pessoas, mensagen);
+                                funcaoCrude.Excluir(pessoas, mensagen);
                                 break;
                             case 4:
-                                Editar(menu, pessoas);
+                                funcaoCrude.Editar(menu, pessoas);
                                 break;
                             case 5:
                                 break;
@@ -116,19 +117,19 @@ namespace ClubeDaLeitura.ConsoleApp1
                         switch (menu.MenuSecundarioEmprestimo())
                         {
                             case 1:
-                                RegistraEmprestimos(revistas, pessoas, acharPosicao, emprestimos);
+                                funcaoCrude.RegistraEmprestimos(revistas, pessoas, acharPosicao, emprestimos);
                                 break;
                             case 2:
-                                MostrarEmprestimos(emprestimos,true);
+                                funcaoCrude.MostrarEmprestimos(emprestimos,true);
                                 break;
                             case 3:
-                                ExcluirEmprestimo(emprestimos, mensagen, pessoas, revistas);
+                                funcaoCrude.ExcluirEmprestimo(emprestimos, mensagen, pessoas, revistas);
                                 break;
                             case 4:
-                               Editar(menu, emprestimos);
+                                funcaoCrude.Editar(menu, emprestimos);
                                 break;
                             case 5:
-                                MostrarEmprestimos(emprestimos, false);
+                                funcaoCrude.MostrarEmprestimos(emprestimos, false);
                                 break;
                             case 6:
                                 break;
@@ -144,128 +145,6 @@ namespace ClubeDaLeitura.ConsoleApp1
                         break;
                 }
             }while (true);
-        }
-
-        private static void MostrarEmprestimos(Emprestimo[] emprestimos,bool abertos)
-        {
-            if (abertos) {
-                for (int i = 0; i < emprestimos.Length; i++)
-                {
-                    if (emprestimos[i] == null || emprestimos[i].aberto == false)
-                        continue;
-                    Console.WriteLine($"ID : {i}");
-                    emprestimos[i].Mostrar();
-                }
-            }
-            else
-            {
-                for (int i = 0; i < emprestimos.Length; i++)
-                {
-                    if (emprestimos[i] == null)
-                        continue;
-                    TimeSpan diff = DateTime.Today - emprestimos[i].dataEmprestimo;
-                    if (diff.Days > 30)
-                        continue;
-                    Console.WriteLine($"ID : {i}");
-                    emprestimos[i].Mostrar();
-                }
-            }
-            Console.ReadKey();
-        }
-
-        private static void RegistraEmprestimos(Revista[] revistas, Pessoa[] pessoas, AcharPosicao acharPosicao, Emprestimo[] emprestimos)
-        {
-            int posicao = acharPosicao.AcharPosicaoNulo(emprestimos);
-            emprestimos[posicao] = new Emprestimo();
-            emprestimos[posicao].Registrar(revistas, pessoas);
-        }
-
-        private static void Editar(Menu menu, dynamic[] objeto)
-        {
-            Mostrar(objeto);
-            int numeroEditar = menu.Editar(objeto);
-            Console.Clear();
-            objeto[numeroEditar].Editar();
-        }
-
-        private static void Registrar(dynamic[] objeto, AcharPosicao acharPosicao)
-        {
-            int posicao = acharPosicao.AcharPosicaoNulo(objeto);
-           
-                if (objeto is Caixa[])
-                    objeto[posicao] = new Caixa();
-            else if (objeto is Pessoa[])
-                objeto[posicao] = new Pessoa();
-            objeto[posicao].Registrar();
-        }
-
-        private static void EditarRevista(Menu menu, Revista[] revista, Caixa[] caixa)
-        {
-            Mostrar(revista);
-            int numeroEditar = menu.Editar(revista);
-            Console.Clear();
-            revista[numeroEditar].EditarRevista(caixa);
-        }
-        private static void ExcluirEmprestimo(Emprestimo[] emprestimo, Mensagen mensagen,Pessoa[] pessoas,Revista[] revistas)
-        {
-            Mostrar(emprestimo);
-            int posicaoExluir = mensagen.Excluir(emprestimo, "qual o ID que deseja excluir");
-
-            emprestimo[posicaoExluir].aberto = false;
-            emprestimo[posicaoExluir].dataDevolucao = DateTime.Now;
-            foreach (var item in pessoas)
-            {
-                if (item == null)
-                    continue;
-                if (item.nome == emprestimo[posicaoExluir].amigo.nome)
-                {
-                    item.temEmprestimo = false;
-                    break;
-                }
-            }
-            foreach (var item in revistas)
-            {
-                if (item == null)
-                    continue;
-                if (item.numeroEdicao == emprestimo[posicaoExluir].revista.numeroEdicao)
-                {
-                    item.disponivel = true;
-                    break;
-                }
-            }
-            mensagen.Sucesso("fechado com sucesso");
-            
-        }
-        private static void Excluir(dynamic[] objeto, Mensagen mensagen)
-        {
-            Mostrar(objeto);
-            int posicaoExluir = mensagen.Excluir(objeto, "qual o ID que deseja excluir");
-            
-         
-                objeto[posicaoExluir] = null;
-                mensagen.Sucesso("removido com sucesso");
-            
-            Console.ReadKey();
-        }
-
-        private static void RegistrarNovaRevista(Revista[] revista, Caixa[] caixa, AcharPosicao acharPosicao)
-        {
-            int posicao = acharPosicao.AcharPosicaoNulo(revista);
-            revista[posicao] = new();
-            revista[posicao].Registar(caixa);
-        }
-
-        private static void Mostrar(dynamic[] objeto)
-        {
-            for (int i = 0; i < objeto.Length; i++)
-            {
-                if (objeto[i] == null)
-                    continue;
-                Console.WriteLine($"ID : {i}");
-                objeto[i].Mostrar();
-            }
-
-            Console.ReadKey();
         }
     }
 }
